32c25b73c2ce219fe1096df227e09a197ea9d563
Kevin Caruso <Kjcaruso12@gmail.com>
2022-02-23 14:51:45 -0600

    added the ability to assign tickets to specific employees

--- src/components/serviceTickets/Ticket.js
+++ src/components/serviceTickets/Ticket.js
@@ -1,28 +1,75 @@
 import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom/cjs/react-router-dom.min"
+import { useParams, useHistory } from "react-router-dom"
 
 export const Ticket = () => {
-    const [ticket, assignTicket] = useState({})
+    const [ticket, assignTicket] = useState({})  // State variable for current ticket object
+    const [employees, syncEmployees] = useState([])  // State variable for array of employees
+    const { ticketId } = useParams()  // Variable storing the route parameter
+    const history = useHistory()
 
-    const { ticketId } = useParams()
 
-
+    // Fetch the individual ticket when the parameter value changes
     useEffect(
         () => {
             return fetch(`http://localhost:8088/serviceTickets/${ticketId}?_expand=customer&_expand=employee`)
-            .then(res => res.json())
-            .then(assignTicket)
+                .then(response => response.json())
+                .then((data) => {
+                    assignTicket(data)
+                })
+
         },
-        [ticketId]
+        [ ticketId ]  // Above function runs when the value of ticketId change
     )
+
+    // Fetch all employees
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/employees`)
+                .then(res => res.json())
+                .then(syncEmployees)
+        },
+        []  // Empty dependency array only reacts to JSX initial rendering
+    )
+
+    // Function to invoke when an employee is chosen from <select> element
+    const assignEmployee = (evt) => {
+
+        // Construct a new object to replace the existing one in the API
+        const updatedTicket = {
+            customerId: ticket.customerId,
+            employeeId: parseInt(evt.target.value),
+            description: ticket.description,
+            emergency: ticket.emergency,
+            dateCompleted: ticket.dateCompleted
+        }
+
+        // Perform the PUT HTTP request to replace the resource
+        fetch(`http://localhost:8088/serviceTickets/${ticketId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedTicket)
+        })
+            .then(() => {
+                history.push("/tickets")
+            })
+    }
 
     return (
         <>
-            <h2>Ticket Details</h2>
             <section className="ticket">
-                <h3 className="ticket_description">{ticket.description}</h3>
-                <div className="ticket_customer">Submitted by {ticket.customer?.first_name} {ticket.customer?.last_name}</div>
-                <div className="ticket_employee">Assigned to {ticket.employee?.first_name} {ticket.employee?.last_name}</div>
+                <h3 className="ticket__description">{ticket.description}</h3>
+                <div className="ticket__customer">Submitted by {ticket.customer?.first_name} {ticket.customer?.last_name}</div>
+                <div className="ticket__employee">Assigned to
+                    <select
+                        value={ ticket.employeeId }
+                        onChange={ assignEmployee }>
+                        {
+                            employees.map(e => <option key={`employee--${e.id}`} value={e.id}>{e.first_name} {e.last_name}</option>)
+                        }
+                    </select>
+                </div>
             </section>
         </>
     )
